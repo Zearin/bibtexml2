1590c4f9239a65ab39539f94456d76002b7acc1a
Zearin <zearin@gonk.net>
2013-07-26 15:54:56 -0400

    formatter.py: Various code cleaning & improvements

--- bibtexml2/formatter.py
+++ bibtexml2/formatter.py
@@ -35,33 +35,39 @@ import xmlwitch
 #   Variables
 #----------------------------------------------------------------------
 DTD     = '<!DOCTYPE bibtex:file PUBLIC "-//BibTeXML//DTD XML for BibTeX v1.0//EN" "bibtexml.dtd">\n'
-XMLNS   = 'http://bibtexml.sf.net/'
-SKIPPABLE_TTYPES = frozenset([Whitespace, Operator, Punctuation])
 XML     = xmlwitch.Builder(version='1.0', encoding='utf-8')
+XMLNS   = 'http://bibtexml.sf.net/'
+
+SKIPPABLE_TTYPES = frozenset([Text.Whitespace, Whitespace, Operator, Punctuation])
+
 
 
 #----------------------------------------------------------------------
 #   Functions
 #----------------------------------------------------------------------
-def _write_entry(entry_token_list):
+def _is_entry(token_tuple):
+    ttype, value = token_tuple[0], token_tuple[1]
+    return ttype == Keyword.Reserved # and str(value).startswith('@')
+    
+def _write_entry(entry_token_stream):
     try:
-        assert( entry_token_list[0][0] is Keyword.Reserved ) # sanity check! be sure we have an entry
+        assert( entry_token_stream[0][0] is Keyword.Reserved ) # sanity check! be sure we have an entry
     except AssertionError:
         import sys
-        sys.exit(entry_token_list[0])
+        sys.exit(entry_token_stream[0])
     
-    entrytype = entry_token_list[0][1]
-    entrytype = entrytype.lstrip('@')
-    entrylabel = (entry_token_list[1][0] is Name.Label) and entry_token_list[1][1] or None
+    entrytype = entry_token_stream[0][1]
+    entrytype = entrytype.lstrip('@').lower()
+    entrylabel = (entry_token_stream[1][0] is Name.Label) and entry_token_stream[1][1] or None
     
     if entrylabel:
         with XML.bibtex__entry( id=entrylabel ):
             with XML['bibtex__{0}'.format(entrytype)]:
-                _write_fields( entry_token_list[2:] )
+                _write_fields( entry_token_stream[2:] )
     else:
         with XML.bibtex__entry():
             with XML['bibtex__{0}'.format(entrytype)]:
-                _write_fields( entry_token_list[1:] )
+                _write_fields( entry_token_stream[1:] )
 
 def _write_fields(field_token_list):
     field_indeces = sorted([idx for idx, item in enumerate(field_token_list) if item[0] is Name.Attribute])
@@ -69,7 +75,7 @@ def _write_fields(field_token_list):
     is_field = lambda x: x[0] is not Name.Attribute
     
     for idx, item in enumerate(field_token_list):
-        ttype, value = item[0], item[1]
+        ttype, value = item[0], item[1].lower().strip(r' \'"}{')
         if ttype is Name.Attribute:
             fieldname = value
             metaidx = field_indeces.index(idx)
@@ -80,7 +86,7 @@ def _write_fields(field_token_list):
                                     is_field, 
                                     field_token_list[idx+1:])
                         ])
-            XML['bibtex__{0}'.format(fieldname)]( value )
+            XML[ 'bibtex__{0}'.format(fieldname) ]( value )
                 
 
 def _entries(token_seq, indeces):
@@ -92,10 +98,11 @@ def _entries(token_seq, indeces):
         start = indeces[idx]
         try:
             stop      = indeces[idx + 1]
-            entries.append( token_seq[start:stop] )
         except IndexError:
-            entries.append( token_seq[start:] )
-        
+            stop      = None
+        finally:
+            entries.append( token_seq[start:stop] )
+    
     return entries
 
 
@@ -107,6 +114,7 @@ class BibTeXML(Formatter):
     
     Output (should) be valid according to the output from the original 
     BibTeXML project (hosted on SourceForge.net).
+    
     '''
     
     name = 'BibTeXML'
@@ -114,39 +122,52 @@ class BibTeXML(Formatter):
     
     
     def __init__(self, **options):
-        Formatter.__init__(self, **options)
+        super(BibTeXML, self).__init__(**options)
 
-        
     def format(self, tokensource, outfile):
         # need to be able to look ahead 
         token_seq = tuple([i for i in tokensource if i[0] not in SKIPPABLE_TTYPES])
         
         # mark where entries occur
-        entry_indeces = tuple([ idx for idx, (ttype, value)  in enumerate(token_seq) 
-                                if  ttype is Keyword.Reserved and 
-                                    value.startswith('@') 
-                            ])
-        
+        entry_indeces = tuple([idx for idx, item in enumerate(token_seq) if _is_entry(item)])
         
         # build list of sub-iterators
         entries = _entries(token_seq, entry_indeces)
-        
+
         # begin XML document
         XML.write(DTD)
         
         with XML.bibtex__file(xmlns__bibtex=XMLNS):
             idx = 0
             
-            # write anything that occurs before the first entry
-            for idx, item in enumerate(token_seq):
-                if idx >= entry_indeces[0]:
-                    break
-                ttype, value = item[0], item[1]
-                XML.write_indented('<!-- {0} -->'.format(value)) # FIXME: temp comment everything to debug
-
-            # write the entries
-            for idx, entry in enumerate( entries ):
-                _write_entry(entry)
+            # make sure we've captured entries
+            if len(entry_indeces):    
+                # write anything that occurs before the first entry
+                
+                in_comment = False
+                for idx, item in enumerate(token_seq):
+                    if idx > entry_indeces[0]:
+                        break
+                    
+                    ttype, value = item[0], item[1]
+                    next_ttype = token_seq[idx+1][0]
+                                        
+                    if ttype is Comment:
+                        if not in_comment:
+                            in_comment = True
+                            XML.write_indented('\n<!--\n')
+                            XML._indentation += 1
+                        XML.write_indented(value.strip('\r\n'))
+                        continue
+                    elif in_comment:
+                        XML.write_indented('\n-->')
+                        XML._indentation -= 1
+                        continue
+                            
+            
+                # write the entries
+                for idx, entry in enumerate( entries ):
+                    _write_entry(entry)
         
         outfile.write(str(XML))
             
